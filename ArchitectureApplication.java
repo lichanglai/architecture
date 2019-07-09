a05b1b92515ae7fefdacbf85242f2940e0c20a87
li.changlai <Li1321330123>
2019-07-09 18:14:00 +0800

    整合

---
+++ ArchitectureApplication.java
@@ -0,0 +1,15 @@
+package com.example.architecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class ArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchitectureApplication.class, args);
+	}
+
+}
